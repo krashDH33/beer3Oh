786a0582994de7ccef1c0f6c557b57ceeea1c470
Sean Hammarstrom <sean.hammarstrom@gmail.com>
2020-11-29 16:55:44 -0800

    Working accordion and Modal, Screen resize

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
+let collArr = ["#hopsA", "#maltA", "#yeastA", "#palateA", "#abvA", "#ibuA"];
+let modArr = ["#hopsMod", "#maltMod", "#yeastMod", "#palateMod", "#abvMod", "#ibuMod"];
+let tarArr = ["#hops", "#malt", "#yeast", "#palate", "#abv", "#ibu"];
+
 $(window).on('load resize', function sizeChanged() {
-    if ($(this).width() >= 574) {
-        $("a[data-toggle='collapse']").attr("data-toggle", "modal");
-        $("a[data-target='#hops']").attr("data-target", "#hopsM");
-    } else {
-        $("a[data-toggle='modal']").attr("data-toggle", "collapse");
-        $("a[data-target='#hopsM']").attr("data-target", "#hops");
+    let i = 0;
+    for (i = 0; i < collArr.length; i++) {
+        if ($(this).width() >= 574) {
+            $("a[data-toggle='collapse']").attr("data-toggle", "modal");
+            $(collArr[i]).attr("data-target", modArr[i]);
+        } else {
+            $("a[data-toggle='modal']").attr("data-toggle", "collapse");
+            $(collArr[i]).attr("data-target", tarArr[i]);
+        }
     }
 });
 
